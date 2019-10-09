9f581cd37749e0a075d45602ef516ff299b205a0
lpc <lpintocastaneda@gmail.com>
2019-10-09 02:13:52 +0200

    trying a different interface

--- src/simpleclock/clock.py
+++ src/simpleclock/clock.py
@@ -15,60 +15,82 @@ class Clock:
     the silent version of a function does not trigger a call (i.e. does not reset elapsed_since_last_call)
     """
 
-    def __init__(self, default_rounding_precision=2):
-        self._init_ref = None
-        self._last_ref = None
+    def __init__(self, default_rounding_precision=2, _timer=time.perf_counter):
+        self._timer = _timer
+        self._times = {
+            "init": None,
+            "last": None
+        }
         self.default_rounding_precision = default_rounding_precision
+        self.elapsed_since_start: UpdatingReader = None
+        self.elapsed_since_last_call: UpdatingReader = None
+        self.silent: ReaderPair = None
 
     @classmethod
-    def started(cls):
-        clock = cls()
+    def started(cls, default_rounding_precision=2):
+        clock = cls(default_rounding_precision)
         clock.start()
         return clock
 
-    def start(self):
-        self._init_ref = time.perf_counter()
-        self._last_ref = self._init_ref
+    def start(self, base_time=None):
+        self._times["init"] = self._timer() if base_time is None else base_time
+        self._times["last"] = self._times["init"]
+        self.elapsed_since_start = UpdatingReader(self, "init", default_comment_start)
+        self.elapsed_since_last_call = UpdatingReader(self, "last", default_comment_last_call)
+        self.silent = ReaderPair(SilentReader(self, "init", default_comment_start),
+                                 SilentReader(self, "last", default_comment_last_call))
 
-    def restart(self):
-        self.start()
+    def restart(self, base_time=None):
+        self._times["init"] = self._timer() if base_time is None else base_time
+        self._times["last"] = self._times["init"]
 
-    def call(self):
-        self._last_ref = time.perf_counter()
 
-    def get_elapsed_since_last_call(self) -> float:
-        prev_last_ref = self._last_ref
-        self.call()
-        return self._last_ref - prev_last_ref
+class BaseReader:
+    def __init__(self, clock, ref_target, default_comment):
+        self.clock = clock
+        self.ref_target = ref_target
+        self.default_comment = default_comment
+
+    def _update(self, now):
+        self.clock._times["last"] = now
+
+    def _get(self, now):
+        return now - self.clock._times[self.ref_target]
+
+    def get(self):
+        raise NotImplementedError
 
     def _print_since(self, elapsed, comment, rounding_precision):
-        rounding_precision = self.default_rounding_precision if rounding_precision is None else rounding_precision
-        print_comment(elapsed, comment, rounding_precision)
+        print_comment(elapsed,
+                      self.default_comment if comment is None else comment,
+                      self.clock.default_rounding_precision if rounding_precision is None else rounding_precision)
         return elapsed
 
-    def print_elapsed_since_last_call(self, comment=default_comment_last_call, rounding_precision=None) -> float:
-        return self._print_since(self.get_elapsed_since_last_call(), comment, rounding_precision)
+    def print(self, comment=None, rounding_precision=None) -> float:
+        return self._print_since(self.get(), comment, rounding_precision)
 
-    def get_elapsed_since_start(self) -> float:
-        self.call()
-        elapsed = self._last_ref - self._init_ref
+
+class UpdatingReader(BaseReader):
+
+    def call(self):
+        self._update(self.clock._timer())
+
+    def get(self):
+        now = self.clock._timer()
+        elapsed = self._get(now)
+        self._update(now)
         return elapsed
 
-    def print_elapsed_since_start(self, comment=default_comment_start, rounding_precision=None) -> float:
-        return self._print_since(self.get_elapsed_since_start(), comment, rounding_precision)
 
-    def get_elapsed_since_last_call_silent(self) -> float:
-        return time.perf_counter() - self._last_ref
+class SilentReader(BaseReader):
+    def get(self):
+        return self._get(self.clock._timer())
 
-    def print_elapsed_since_last_call_silent(self, comment=default_comment_last_call, rounding_precision=None) -> float:
-        return self._print_since(self.get_elapsed_since_last_call_silent(), comment, rounding_precision)
 
-    def get_elapsed_since_start_silent(self) -> float:
-        elapsed = time.perf_counter() - self._init_ref
-        return elapsed
-
-    def print_elapsed_since_start_silent(self, comment=default_comment_start, rounding_precision=None) -> float:
-        return self._print_since(self.get_elapsed_since_start_silent(), comment, rounding_precision)
+class ReaderPair:
+    def __init__(self, since_start, since_last_call):
+        self.elapsed_since_start: BaseReader = since_start
+        self.elapsed_since_last_call: BaseReader = since_last_call
 
 
 def format_time(t: float, rounding_precision) -> str:
--- tests/general_test.py
+++ tests/general_test.py
@@ -6,9 +6,45 @@ def test_sleep(duration=0.1):
     clock = simpleclock.Clock.started()
     time.sleep(duration)
 
-    assert clock.get_elapsed_since_last_call() >= duration
-    assert clock.get_elapsed_since_start() >= duration
+    assert clock.elapsed_since_last_call.get() >= duration
+    assert clock.elapsed_since_start.get() >= duration
 
     time.sleep(duration)
 
-    assert clock.get_elapsed_since_start() >= 2 * duration
+    assert clock.elapsed_since_start.get() >= 2 * duration
+
+
+def test_silent_sleep(duration=0.1):
+    clock = simpleclock.Clock.started()
+    time.sleep(duration)
+
+    assert clock.silent.elapsed_since_start.get() > duration
+    assert clock.silent.elapsed_since_last_call.get() > duration
+
+    time.sleep(duration)
+
+    assert clock.elapsed_since_last_call.get() > 2 * duration
+
+
+def test_get():
+    timer_values = iter((1, 2, 3, 4))
+    clock = simpleclock.Clock(_timer=lambda: next(timer_values))
+    clock.start(base_time=0)
+
+    assert clock.silent.elapsed_since_start.get() == 1
+    assert clock.silent.elapsed_since_last_call.get() == 2
+    assert clock.elapsed_since_start.get() == 3
+    assert clock.elapsed_since_last_call.get() == 1
+
+
+def test_print(capsys):
+    timer_values = iter((0, 0, 2.82, 10))
+    clock = simpleclock.Clock(_timer=lambda: next(timer_values), default_rounding_precision=0)
+    clock.start()
+    clock.elapsed_since_start.get()
+
+    clock.elapsed_since_last_call.print("Test", rounding_precision=2)
+    assert capsys.readouterr().out == "Test: 2.82s" + "\n"
+
+    clock.elapsed_since_last_call.print("Another test")
+    assert capsys.readouterr().out == "Another test: 7s" + "\n"
