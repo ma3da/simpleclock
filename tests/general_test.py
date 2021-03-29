ce5b55af874e07f66a0853dd049813c740ba7e23
lpc <lpintocastaneda@gmail.com>
2021-03-29 23:44:56 +0200

    updatish tests

--- tests/general_test.py
+++ tests/general_test.py
@@ -1,67 +1,30 @@
 import time
-import simpleclock
+import simpleclock as sc
 
 
 def test_sleep(duration=0.1):
-    clock = simpleclock.Clock.started()
-    time.sleep(duration)
-
-    assert clock.elapsed_since_last_call() >= duration
-    assert clock.elapsed_since_start() >= duration
+    since = sc.Since()
+    ts = sc.ts()
 
     time.sleep(duration)
 
-    assert clock.elapsed_since_start() >= 2 * duration
-
-
-def test_silent_sleep(duration=0.1):
-    clock = simpleclock.Clock.started()
-    time.sleep(duration)
-
-    assert clock.silent.elapsed_since_start() > duration
-    assert clock.silent.elapsed_since_last_call() > duration
+    assert since(ts).duration >= duration
 
     time.sleep(duration)
 
-    assert clock.elapsed_since_last_call() > 2 * duration
-
-
-def test_call(duration=0.1):
-    clock = simpleclock.Clock.started()
-    time.sleep(duration)
-
-    clock.call()
-    assert (clock.silent.elapsed_since_start()
-            > clock.silent.elapsed_since_last_call())
-
-
-def test_get():
-    timer_values = iter((1, 2, 3, 4))
-    clock = simpleclock.Clock(timer=lambda: next(timer_values))
-    clock.start(base_time=0)
-
-    # .__call__() is defined as .get()
-    assert clock.silent.elapsed_since_start() == 1
-    assert clock.silent.elapsed_since_last_call() == 2
-    assert clock.elapsed_since_start() == 3
-    assert clock.elapsed_since_last_call() == 1
+    assert since(ts).duration >= 2 * duration
 
 
 def test_print(capsys):
-    timer_values = iter((0, 0, 2.82, 10))
-    clock = simpleclock.Clock(timer=lambda: next(timer_values), default_rounding_precision=0)
-    clock.start()
-    clock.elapsed_since_start()
+    since = sc.Since(precision=0)
+    ts = sc.ts("start")
 
-    clock.elapsed_since_last_call.print("Test", rounding_precision=2)
-    assert capsys.readouterr().out == "Test: 2.82s" + "\n"
+    time.sleep(0.01)
 
-    clock.elapsed_since_last_call.print("Another test")
-    assert capsys.readouterr().out == "Another test: 7s" + "\n"
+    since(ts).print()
+    assert capsys.readouterr().out == "start: 0s\n"
 
+    time.sleep(.5)
 
-def test_default_comment(capsys):
-    clock = simpleclock.Clock(timer=lambda: 5, default_comment_start="it has been running for")
-    clock.start(base_time=0)
-    clock.elapsed_since_start.print()
-    assert capsys.readouterr().out == "it has been running for: 5.00s" + "\n"
+    since(ts).print("and now")
+    assert capsys.readouterr().out == "and now: 1s\n"
